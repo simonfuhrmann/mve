d826a08ffb8d1a5ae68d3b441048f1720c98f9fe
Simon Fuhrmann <simonfuhrmann@gmx.de>
2012-12-16 20:05:16 -0800

    SFM: Moved AlignedMemory to util

--- libs/sfm/_test_matching.cc
+++ libs/sfm/_test_matching.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "util/alignedmemory.h"
 #include "util/timer.h"
 #include "mve/image.h"
 #include "mve/imagetools.h"
@@ -8,33 +9,6 @@
 #include "surf.h"
 #include "matching.h"
 #include "matchingvisualizer.h"
-
-// TODO: Move to its own file in util.
-template <typename T, uintptr_t MODULO = 16>
-struct AlignedArray
-{
-    AlignedArray (void) : raw(0), aligned(0) {}
-    AlignedArray (std::size_t size) { this->allocate(size); }
-    ~AlignedArray (void) { this->deallocate(); }
-    void allocate (std::size_t size)
-    {
-        this->raw = new unsigned char[(size + MODULO - 1) * sizeof(T)];
-        uintptr_t tmp = reinterpret_cast<uintptr_t>(this->raw);
-        this->aligned = (tmp & ~(MODULO - 1)) == tmp
-            ? reinterpret_cast<T*>(tmp)
-            : reinterpret_cast<T*>((tmp + MODULO) & ~(MODULO - 1));
-    }
-    void deallocate (void)
-    {
-        delete this->raw;
-        this->raw = 0;
-        this->aligned = 0;
-    }
-
-    unsigned char* raw;
-    T* aligned;
-};
-
 
 int
 main (int argc, char** argv)
@@ -86,17 +60,17 @@ main (int argc, char** argv)
     /*
      * Convert the descriptors to aligned short arrays.
      */
-    AlignedArray<short> match_descr_1(64 * descr1.size());
-    AlignedArray<short> match_descr_2(64 * descr2.size());
+    util::AlignedMemory<short, 16> match_descr_1(64 * descr1.size());
+    util::AlignedMemory<short, 16> match_descr_2(64 * descr2.size());
 
-    short* mem_ptr = match_descr_1.aligned;
+    short* mem_ptr = match_descr_1.begin();
     for (std::size_t i = 0; i < descr1.size(); ++i)
     {
         std::vector<float> const& d = descr1[i].data;
         for (int j = 0; j < 64; ++j)
             *(mem_ptr++) = static_cast<short>(d[j] * 127.0f);
     }
-    mem_ptr = match_descr_2.aligned;
+    mem_ptr = match_descr_2.begin();
     for (std::size_t i = 0; i < descr2.size(); ++i)
     {
         std::vector<float> const& d = descr2[i].data;
@@ -112,8 +86,8 @@ main (int argc, char** argv)
     sfm::MatchingResult matchresult;
 
     util::WallTimer timer;
-    sfm::match_features(matchopts, match_descr_1.aligned, descr1.size(),
-        match_descr_2.aligned, descr2.size(), &matchresult);
+    sfm::match_features(matchopts, match_descr_1.begin(), descr1.size(),
+        match_descr_2.begin(), descr2.size(), &matchresult);
     sfm::remove_inconsistent_matches(&matchresult);
     std::cout << "Two-view matching took " << timer.get_elapsed() << "ms." << std::endl;
 
