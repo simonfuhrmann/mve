84d6298de7408ee719431445b1a207c1bb242053
Simon Fuhrmann <simonfuhrmann@gmx.de>
2016-10-27 20:18:11 +0200

    APPS: Prepare makescene for more bundle importers

--- apps/makescene/makescene.cc
+++ apps/makescene/makescene.cc
@@ -424,20 +424,8 @@ namespace
 }
 
 void
-import_bundle (AppSettings const& conf)
+import_bundle_noah_ps (AppSettings const& conf)
 {
-    /**
-     * Try to detect VisualSFM bundle format.
-     * In this case the input is a file with extension ".nvm".
-     */
-    if (util::string::right(conf.input_path, 4) == ".nvm"
-        && util::fs::file_exists(conf.input_path.c_str()))
-    {
-        std::cout << "Info: Detected VisualSFM bundle format." << std::endl;
-        import_bundle_nvm(conf);
-        return;
-    }
-
     /* Build some paths. */
     std::string bundle_fname;
     std::string imglist_file;
@@ -459,10 +447,7 @@ import_bundle (AppSettings const& conf)
         undist_path = util::fs::join_path(conf.input_path, PS_UNDIST_DIR);
 
         if (util::fs::file_exists(bundle_fname.c_str()))
-        {
-            std::cout << "Info: Detected Photosynther format." << std::endl;
             bundler_fmt = BUNDLE_FORMAT_PHOTOSYNTHER;
-        }
     }
 
     /*
@@ -483,10 +468,7 @@ import_bundle (AppSettings const& conf)
         image_path = util::fs::join_path(conf.input_path, BUNDLER_IMAGE_DIR);
 
         if (util::fs::file_exists(bundle_fname.c_str()))
-        {
-            std::cout << "Info: Detected Noah's Bundler format." << std::endl;
             bundler_fmt = BUNDLE_FORMAT_NOAH_BUNDLER;
-        }
     }
 
     /* Read bundle file. */
@@ -534,7 +516,7 @@ import_bundle (AppSettings const& conf)
         }
         if (orig_files.size() != bundle->get_num_cameras())
         {
-            std::cerr << "Error: Invalid amount of original images." << std::endl;
+            std::cerr << "Error: False amount of original images." << std::endl;
             std::exit(EXIT_FAILURE);
         }
         std::cout << "Recognized " << orig_files.size()
@@ -730,6 +712,66 @@ import_bundle (AppSettings const& conf)
         << num_valid_cams << " valid cameras." << std::endl;
     std::cout << "Imported " << undist_imported
         << " undistorted images." << std::endl;
+}
+
+/* ---------------------------------------------------------------- */
+
+bool
+is_visual_sfm_bundle_format (AppSettings const& conf)
+{
+    return util::string::right(conf.input_path, 4) == ".nvm"
+        && util::fs::file_exists(conf.input_path.c_str());
+}
+
+bool
+is_photosynther_bundle_format (AppSettings const& conf)
+{
+    std::string bundle_fname = util::fs::join_path(conf.bundle_path,
+        "synth_" + util::string::get(conf.bundle_id) + ".out");
+    return util::fs::file_exists(bundle_fname.c_str());
+}
+
+bool
+is_noah_bundler_format (AppSettings const& conf)
+{
+    std::string bundle_fname = util::fs::join_path(conf.bundle_path,
+        conf.bundle_id == 0 ? "bundle.out" : "bundle_"
+        + util::string::get_filled(conf.bundle_id, 3, '0') + ".out");
+    return util::fs::file_exists(bundle_fname.c_str());
+}
+
+/* ---------------------------------------------------------------- */
+
+void
+import_bundle (AppSettings const& conf)
+{
+    /**
+     * Try to detect VisualSFM bundle format.
+     * In this case the input is a file with extension ".nvm".
+     */
+    if (is_visual_sfm_bundle_format(conf))
+    {
+        std::cout << "Info: Detected VisualSFM bundle format." << std::endl;
+        import_bundle_nvm(conf);
+        return;
+    }
+
+    /**
+     * Try to detect Noah bundler or Photosynther. These bundle formats
+     * are similar and handled with the same import function.
+     */
+    if (is_photosynther_bundle_format(conf))
+    {
+        std::cout << "Info: Detected Photosynther bundle format." << std::endl;
+        import_bundle_noah_ps(conf);
+        return;
+    }
+    if (is_noah_bundler_format(conf))
+    {
+        std::cout << "Info: Detected Noah bundler format." << std::endl;
+        import_bundle_noah_ps(conf);
+        return;
+    }
 }
 
 /* ---------------------------------------------------------------- */
