2a179cd412d89ef56fb202a6add3ce1ce6f4621c
Simon Fuhrmann <simonfuhrmann@gmx.de>
2017-01-23 09:27:13 -0800

    APPS: Change sceneupgrade license to GPL

--- apps/sceneupgrade/sceneupgrade.cc
+++ apps/sceneupgrade/sceneupgrade.cc
@@ -4,7 +4,7 @@
  * All rights reserved.
  *
  * This software may be modified and distributed under the terms
- * of the BSD 3-Clause license. See the LICENSE.txt file for details.
+ * of the GPL 3 license. See the LICENSE.txt file for details.
  */
 
 #include <cstring>
