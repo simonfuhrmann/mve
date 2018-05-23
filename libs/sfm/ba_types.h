2381429cbef09546e6ab99e15d290e7354952511
Antony Ducommun <antony@aducommun.ch>
2018-05-23 23:50:27 +0800

    Add principal point intrinsic to ba Camera & Viewport

--- libs/sfm/ba_types.h
+++ libs/sfm/ba_types.h
@@ -15,6 +15,7 @@ struct Camera
 
     double focal_length = 0.0;
     double distortion[2];
+    double principal_point[2];
     double translation[3];
     double rotation[9];
     bool is_constant = false;
@@ -41,6 +42,7 @@ inline
 Camera::Camera (void)
 {
     std::fill(this->distortion, this->distortion + 2, 0.0);
+    std::fill(this->principal_point, this->principal_point + 2, 0.0);
     std::fill(this->translation, this->translation + 3, 0.0);
     std::fill(this->rotation, this->rotation + 9, 0.0);
 }
