247ea308f9221c9c88f0950aab19e5d2348aca23
Andre Schulz <andre@exhale.de>
2024-06-28 18:07:25 +0200

    MVE: Replace vsprintf() with vsnprintf()
    Apparently, vsprintf() is deprecated on macOS. Fixes the following
    compiler warning when compiling with Apple clang 14.0.0 (clang-1400.0.29.202)
    on macOS 12:
    
      clang++ -Wall -Wextra -Wundef -pedantic -march=native -funsafe-math-optimizations -fno-math-errno -std=c++14 -g -O3 -pthread -DGL_SILENCE_DEPRECATION=1 -fPIC -I../../libs `pkg-config --cflags libjpeg` `pkg-config --cflags libpng` `pkg-config --cflags libtiff-4`  -c -o image_io.o image_io.cc
      image_io.cc:634:7: warning: 'vsprintf' is deprecated: This function is provided for compatibility reasons only.  Due to security concerns inherent in the design of sprintf(3), it is highly recommended that you use vsnprintf(3) instead. [-Wdeprecated-declarations]
          ::vsprintf(msg, fmt, ap);
            ^
      /Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/stdio.h:207:1: note: 'vsprintf' has been explicitly marked deprecated here

--- libs/mve/image_io.cc
+++ libs/mve/image_io.cc
@@ -631,7 +631,7 @@ void
 tiff_error_handler (char const* /*module*/, char const* fmt, va_list ap)
 {
     char msg[2048];
-    ::vsprintf(msg, fmt, ap);
+    ::vsnprintf(msg, 2048, fmt, ap);
     throw util::Exception(msg);
 }
 
