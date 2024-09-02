simonfuhrmann/mve

commit 247ea308f9221c9c88f0950aab19e5d2348aca23

MVE: Replace vsprintf() with vsnprintf()
Apparently, vsprintf() is deprecated on macOS. Fixes the following
compiler warning when compiling with Apple clang 14.0.0 (clang-1400.0.29.202)
on macOS 12:

  clang++ -Wall -Wextra -Wundef -pedantic -march=native -funsafe-math-optimizations -fno-math-errno -std=c++14 -g -O3 -pthread -DGL_SILENCE_DEPRECATION=1 -fPIC -I../../libs `pkg-config --cflags libjpeg` `pkg-config --cflags libpng` `pkg-config --cflags libtiff-4`  -c -o image_io.o image_io.cc
  image_io.cc:634:7: warning: 'vsprintf' is deprecated: This function is provided for compatibility reasons only.  Due to security concerns inherent in the design of sprintf(3), it is highly recommended that you use vsnprintf(3) instead. [-Wdeprecated-declarations]
      ::vsprintf(msg, fmt, ap);
        ^
  /Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/stdio.h:207:1: note: 'vsprintf' has been explicitly marked deprecated here

libs/mve/image_io.cc
@@ -631,7 +631,7 @@ void
 tiff_error_handler (char const* /*module*/, char const* fmt, va_list ap)
 {
     char msg[2048];
-    ::vsprintf(msg, fmt, ap);
+    ::vsnprintf(msg, 2048, fmt, ap);
     throw util::Exception(msg);
 }
 

commit a4bb65f1b1f4b34dc24e54c01c9e60fc8ad43cac

MVE: Don't hardcode vsnprintf() destination buffer size
Use sizeof() instead of hardcoding destination buffer size when calling
vsnprintf().

Fixes: 247ea308f922 ("MVE: Replace vsprintf() with vsnprintf()")

libs/mve/image_io.cc
@@ -631,7 +631,7 @@ void
 tiff_error_handler (char const* /*module*/, char const* fmt, va_list ap)
 {
     char msg[2048];
-    ::vsnprintf(msg, 2048, fmt, ap);
+    ::vsnprintf(msg, sizeof(msg), fmt, ap);
     throw util::Exception(msg);
 }
 